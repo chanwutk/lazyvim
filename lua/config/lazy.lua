a5ab9ad5b009cc12faf57e793daa928e2f7a9cef
Chanwut (Mick) Kittivorawong <30903997+chanwutk@users.noreply.github.com>
2025-09-25 17:43:51 -0700

    Remove default options from LazyVim configuration

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -19,14 +19,7 @@ require("lazy").setup({
     -- add LazyVim and import its plugins
     {
       "LazyVim/LazyVim",
-      import = "lazyvim.plugins",
-      opts = {
-        defaults = {
-          autocmds = true, -- lazyvim.config.autocmds
-          keymaps = false, -- lazyvim.config.keymaps
-          options = true, -- lazyvim.config.options
-        }
-      }
+      import = "lazyvim.plugins"
     },
     -- import/override with your plugins
     { import = "plugins" },
